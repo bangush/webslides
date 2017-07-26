625bc6a6f77703d5d1116bb0878263d9b093f212
chiwawaweb <git@chiwawaweb.com>
2017-07-26 18:54:20 +0200

    Mise à jour Assembly

--- SlideShow/Properties/AssemblyInfo.cs
+++ SlideShow/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("SlideShow")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("Diaporama connecté")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("Hewlett-Packard Company")]
+[assembly: AssemblyCompany("Chiwawaweb.com")]
 [assembly: AssemblyProduct("SlideShow")]
-[assembly: AssemblyCopyright("Copyright © Hewlett-Packard Company 2017")]
+[assembly: AssemblyCopyright("Copyright © 2017 Frédéric Schwartz")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.0.1.0")]
+[assembly: AssemblyFileVersion("0.0.1.0")]
