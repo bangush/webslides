e0e6cdf5a4fe250bf6ea02169c0fde068c6c835a
chiwawaweb <git@chiwawaweb.com>
2017-07-27 08:51:06 +0200

    Mise à jour PHP

---
+++ public_html/index.php
@@ -0,0 +1,18 @@
+<?php
+    $idSlide = $_GET['ID'];
+?>
+<!DOCTYPE HTML>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>SlideShow</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+    <body>
+        
+    <img src="slides/INFO.jpg" width=100%>
+    
+    </body>
+</html>
+
+
