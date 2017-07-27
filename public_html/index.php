eb9794a7fff181f0510eff9da04237557c33a55d
chiwawaweb <git@chiwawaweb.com>
2017-07-27 21:59:19 +0200

    Mise à jour PHP

--- public_html/index.php
+++ public_html/index.php
@@ -1,17 +1,40 @@
 <?php
+    $idSlideMax = 18;
+    $slidePath = "slides";
+    $slideAbsPath = getcwd();
     $idSlide = $_GET['ID'];
+    
+    // vérifie si idSlide existe ou ne depasse pas la limite idSlideMax
+    if ($idSlide<1 OR $idSlide>$idSlideMax)
+    {
+        $idSlide=1;
+    }
+    
+    // vérifie si le fichier existe  
+    $slideFile = $slidePath."/slide_".$idSlide.".jpg";    
+    $slideFileExists = $slideAbsPath."/".$slidePath."/slide_".$idSlide.".jpg";
+    
+    if (file_exists($slideFileExists)) {
+        // existe, on affiche l'image
+        //echo "Le fichier existe.";
+        $idSlideNext = $idSlide+1;
+    } else {
+        // n'existe pas on passe à la suivante
+        //echo "Le fichier n'existe pas.";
+        $idSlide++;
+        $idSlideNext = $idSlide;
+    }
 ?>
 <!DOCTYPE HTML>
 <html>
     <head>
         <meta charset="utf-8">
         <title>SlideShow</title>
+        <meta http-equiv="Refresh" content="5;index.php?ID=<?= $idSlideNext; ?>">
         <link rel="stylesheet" href="style.css">
     </head>
     <body>
-        
-    <img src="slides/INFO.jpg" width=100%>
-    
+            <div class="element"><img src="<?php echo $slideFile; ?>" width=100%></div>
     </body>
 </html>
 
