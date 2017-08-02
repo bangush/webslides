59a2d432d5319b1ce5f362d269c174855c3ba550
chiwawaweb <git@chiwawaweb.com>
2017-08-02 16:56:00 +0200

    Mise à jour

---
+++ WebSlides/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WebSlides
+{
+    static class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            String[] arguments = Environment.GetCommandLineArgs();
+            string action = arguments[1].Substring(0, 2);
+
+            if (action == "/s")
+            {
+                Application.Run(new MainFrm());
+            }
+            else
+            {
+                Application.Run(new Splash());
+            }
+                
+        }
+    }
+}
---
+++ WebSlides/Splash.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WebSlides
+{
+    public partial class Splash : Form
+    {
+        public Splash()
+        {
+            InitializeComponent();
+        }
+
+        private void Splash_Load(object sender, EventArgs e)
+        {
+            String[] arguments = Environment.GetCommandLineArgs();
+            string action = arguments[1].Substring(0,2);
+
+            if (action == "/c")
+            {
+                // mode configuration
+                MessageBox.Show("Aucune configuration possible pour cet écran de veille.");
+                this.Close();
+            }
+            else if (action == "/p")
+            {
+                // mode aperçu
+                // ferme le Splash
+                this.Close();
+            }
+            else
+            {
+                // mode Show
+                label1.Text = "Show "+action;
+                
+
+                // bascule sur le slideshow
+                MainFrm frm = new MainFrm();
+                frm.Show();
+
+                // ferme la fenêtre
+                this.Close();
+
+
+            }
+
+
+        }
+
+    }
+}
