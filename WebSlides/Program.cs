ea32dfb25e4caf18cd9841481870577057303222
chiwawaweb <git@chiwawaweb.com>
2017-08-03 08:48:27 +0200

    Mise à jour

--- WebSlides/Program.cs
+++ WebSlides/Program.cs
@@ -24,16 +24,29 @@ namespace WebSlides
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
 
-
-            
-
-
             String[] arguments = Environment.GetCommandLineArgs();
             //action = arguments[1].Substring(0, 2);
-            action = "GetCommandLineArgs: {0} "+ String.Join(", ", arguments);
-            MessageBox.Show(action);
+            
+            // nombre d'arguments envoyé à l'éxécutable
+            int nbArguments = arguments.Length;
 
-            if (action == "/s")
+            if (nbArguments > 1)
+            {
+                action = arguments[1].Substring(0, 2);
+            }
+            else
+            {
+                string dateActuelle = System.DateTime.Now.ToShortDateString();
+                string anneeActuelle = dateActuelle.Substring(6, 4);
+
+            
+                MessageBox.Show("(c) Copyright "+anneeActuelle+", Frédéric Schwartz ");
+                Application.Exit();
+            }
+
+            //MessageBox.Show(Convert.ToString(nbArguments)+" ["+action+"]");
+
+            if (action == "/s" || action == "/S")
             {
                 // Mode Show, on lance le slideshow
                 Application.Run(new MainFrm());
@@ -42,27 +55,28 @@ namespace WebSlides
             {
                 // Page de configuration
 
+                /*
                 SetupFrm frm = new SetupFrm();
                 frm.ShowDialog();
-
-                /*MessageBox.Show(
-                    "Cet écran de veille n'est pas configurable.",
+                */
+                MessageBox.Show(
+                    "Cet écran de veille n'est pas configurable actuellement.",
                     "WebSlides", 
                     MessageBoxButtons.OK, 
-                    MessageBoxIcon.Asterisk);*/
+                    MessageBoxIcon.Asterisk);
             }
 
             else
             {
                 // Sinon on crée le fichier de config et on ferme l'appli.
-                StreamWriter file = new StreamWriter(@"WebSlides.ini");
+                //StreamWriter file = new StreamWriter(@"WebSlides.ini");
 
                 // vérifie si le fichier de config existe déjà
 
 
                 //MessageBox.Show("Le fichier a bien été créé dans l'emplacement spécifié!", "ok", MessageBoxButtons.OK, MessageBoxIcon.Information);
-                file.Write("http://webslides.chiwawaweb.com");
-                file.Close();
+                /*file.Write("http://webslides.chiwawaweb.com");
+                file.Close();*/
 
                 // fermeture de l'application
                 Application.Exit();    
