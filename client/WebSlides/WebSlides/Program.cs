398374fd01cc664e9ca95e89713fa184f4e957b0
chiwawaweb <git@chiwawaweb.com>
2017-08-09 22:46:49 +0200

    Vérification de la connexion internet

--- client/WebSlides/WebSlides/Program.cs
+++ client/WebSlides/WebSlides/Program.cs
@@ -1,22 +1,58 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Runtime;
+using System.Runtime.InteropServices;
 
 namespace WebSlides
 {
     static class Program
     {
+
+        [DllImport("wininet.dll")]
+        private extern static bool InternetGetConnectedState(out int Description, int ReservedValue);
+
+
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
         [STAThread]
         static void Main()
         {
+            
+
+            
+
+            Console.Write("Vérification de la connexion Internet : ");
+
+            int Desc;
+            if (InternetGetConnectedState(out Desc, 0))
+            {
+                // connexion internet OK
+                Console.Write("OK \n");
+            }
+            else
+            {
+                // pas de connexion internet
+                Console.Write("Pas de connexion ! \n");
+            }
+
+
+            Console.ReadLine();
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            //Application.Run(new Form1());
+
+
         }
+
+        
     }
 }
